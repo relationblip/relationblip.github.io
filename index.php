fc549beb2bfee0a166ec5cfec70aaa83123d0971
relationblip <102649193+relationblip@users.noreply.github.com>
2023-03-25 09:37:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?PHP
+$ch = curl_init('https://md-solutions.webflow.io/');
+curl_exec($ch);
+curl_close($ch);
+?>
+
+<style type="text/css">
+.w-webflow-badge {
+display: none !important;
+}
+</style>
